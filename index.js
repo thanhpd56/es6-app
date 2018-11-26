4865b4cfa93fbbdf4e390a3f44a86b09c048fbbc
PhanThanh <thanh.pd@teko.vn>
2018-11-26 07:16:03 +0700

    jquery sample

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import readingTime from "reading-time";
+import frametalk from 'frametalk';
+import $ from 'jquery';
+
+$(document).ready(function () {
+    console.log('document is ready');
+    console.log($('#edit-frame').attr('src'));
+    $('#myid').addClass('selected');
+
+    $('li').eq(1).click(function () {
+        $('#myid').addClass('highlight');
+    })
+})
 
 window.calcRT = ev => {
 	var stats = readingTime(ev.value).text;
