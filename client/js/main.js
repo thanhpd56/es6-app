144553f09c4e2733bcb6679653feae54c3514385
PhanThanh <thanh.pd@teko.vn>
2018-11-27 16:10:37 +0700

    refactor

--- client/js/main.js
+++ client/js/main.js
@@ -3,24 +3,30 @@ import pm from 'post-robot';
 import elementSelectorHandler from './elementSelectorHandler';
 import elementMoveHandler from './elementMoveHandler';
 
-function setupListeners() {
-    pm.on('setEditMode', (event, data) => {
-        elementSelectorHandler.addElementOverlay();
-        elementSelectorHandler.setEventHandler();
-    });
+class Customization {
+    constructor(){
+        $(document).ready(() => {
+            console.log('frame is ready');
+            elementMoveHandler.setElementSelectEventHandler = elementSelectorHandler.setEventHandler;
+            this.setupListeners();
+        });
+    }
 
-    pm.on('prepareElementForMove', (event) => {
-        pm.send(window.parent, 'removeMenu');
-        pm.send(window.parent, 'removePageOverlay');
-        elementSelectorHandler.unsetEventHandlers();
-        const selectedElement = event.data;
-        elementMoveHandler.prepareForMove(selectedElement);
-    });
-    pm.on('removeElemOverlay', elementSelectorHandler.removeElemOverlay);
+    setupListeners = () => {
+        pm.on('setEditMode', (event, data) => {
+            elementSelectorHandler.addElementOverlay();
+            elementSelectorHandler.setEventHandler();
+        });
+
+        pm.on('prepareElementForMove', (event) => {
+            pm.send(window.parent, 'removeMenu');
+            pm.send(window.parent, 'removePageOverlay');
+            elementSelectorHandler.unsetEventHandlers();
+            const selectedElement = event.data;
+            elementMoveHandler.prepareForMove(selectedElement);
+        });
+        pm.on('removeElemOverlay', elementSelectorHandler.removeElemOverlay);
+    }
 }
 
-$(document).ready(function () {
-    console.log('frame is ready');
-    elementMoveHandler.setElementSelectEventHandler = elementSelectorHandler.setEventHandler;
-    setupListeners();
-});
+export default new Customization();
